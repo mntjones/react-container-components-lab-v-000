25db4c0e9e2cdc5272045f624c2cd4073b7576d8
Monica Tamaru Jones <mntamaru@gmail.com>
2018-11-04 15:20:37 +0000

    automatically backed up by learn

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -9,7 +9,7 @@ const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
 // Code LatestMovieReviewsContainer Here
 class LatestMovieReviewsContainer extends Component {
   state = {
-    reviews: [];
+    reviews: []
   }
   
   componentDidMount() {
