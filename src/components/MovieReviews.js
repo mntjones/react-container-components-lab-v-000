76bbd4310fced0b9c1c19897152187dd251ea90b
Monica Tamaru Jones <mntamaru@gmail.com>
2018-11-04 15:12:23 +0000

    automatically backed up by learn

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,9 +1,9 @@
 // Code MovieReviews Here
 import React from 'react';
 
-const MovieReviews = ({ reviews }) => (
-  <div class="review-list">
+const MovieReviews = ({ reviews }) => {
+  <div className="review-list">
     <img src={ img_url } />
     <h3>{ title }</h3>
   </div>
-)
+}
