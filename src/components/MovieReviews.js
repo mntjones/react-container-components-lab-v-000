e12e8cfd8a7ec3ad307cc500a1a917db1af7595f
Monica Tamaru Jones <mntamaru@gmail.com>
2018-11-04 15:14:02 +0000

    automatically backed up by learn

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -4,7 +4,9 @@ import React from 'react';
 const MovieReviews = ({ reviews }) => {
   <div className="review-list">
     <ul>
-      
+      // list the 
+      { reviews.map( (review, idx) => <div className="review" key = { idx }>
+      <li>{ review.display_title }</li></div>)}
     </ul>
   </div>
 }
